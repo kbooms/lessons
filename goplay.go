b9494d796f22059be0e6e51cc21c42a47c174b54
kbooms <mrkbooms@gmail.com>
2025-03-20 13:46:46 -0400

    variables and types

---
+++ goplay.go
@@ -0,0 +1,43 @@
+package main
+
+import "fmt"
+
+// capital first letter makes it a Public variable
+const LoginToken string = "asd09825hjcasd09u72"
+
+func main() {
+	var username string = "kb"
+	fmt.Println(username)
+	fmt.Printf("Variable is of type: %T \n", username)
+
+	var isLoggedIn bool = false
+	fmt.Println(isLoggedIn)
+	fmt.Printf("Variable is of type: %T \n", isLoggedIn)
+
+	var smallVal uint8 = 255
+	fmt.Println(smallVal)
+	fmt.Printf("Variable is of type: %T \n", smallVal)
+
+	var smallFloat float64 = 255.242345678090976543356
+	fmt.Println(smallFloat)
+	fmt.Printf("Variable is of type: %T \n", smallFloat)
+
+	//default values and some aliases
+	var anotherVariable int // this is initialized
+	fmt.Println(anotherVariable)
+	fmt.Printf("Variable is of type: %T \n", anotherVariable)
+
+	// implicit type
+
+	var website = "kbooms.github.io"
+	fmt.Println(website)
+	fmt.Printf("Variable is of type: %T \n", website)
+
+	// no var style
+	numberOfUser := 300000
+	fmt.Println(numberOfUser)
+	fmt.Printf("Variable is of type: %T \n", numberOfUser)
+
+	fmt.Println(LoginToken)
+	fmt.Printf("Variable is of type: %T \n", LoginToken)
+}
