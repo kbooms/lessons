15ec01fb8e956476260356f3972abab3ffcaadc8
kbooms <mrkbooms@gmail.com>
2025-03-24 11:01:35 -0400

    comma ok err syntax

---
+++ 03userinput/main.go
@@ -0,0 +1,21 @@
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"os"
+)
+
+func main() {
+	welcome := "Welcome to User Input"
+	fmt.Println(welcome)
+
+	reader := bufio.NewReader(os.Stdin)
+	fmt.Println("Enter the rating for our Pizza: ")
+
+	// comma ok || err ok syntax
+
+	input, _ := reader.ReadString('\n')
+	fmt.Println("Thanks for rating, ", input)
+	fmt.Printf("type of this rating is %T ", input)
+}
